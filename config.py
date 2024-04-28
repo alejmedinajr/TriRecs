7bf1e02bf98caacdca54f0afe8df65b87dc7df61
Alejandro Medina <alex.m@alexmedina.com>
2024-04-27 23:58:31 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-SPOTIFY_WEBSITE = "https://localhost:8888/website"
-SPOTIFY_REDIRECT = "https://localhost:8888/callback"
-SPOTIFY_CLIENT_ID = "97fe26ec309b4843969ffaf20e13f140"
-SPOTIFY_CLIENT_SECRET = "38f9b268a7c4499f851db32d904d0d45"
-USER = "'ahhhhhhhhhhhhhhhhhhhhhlejandro'"
